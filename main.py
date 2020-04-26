f8ffbda5a4f6f349d2612acb54b8cbf29f036e67
Корнаев <n.kornaev@gmail.com>
2020-04-26 10:39:20 +0300

    первый рабочий вариант

--- main.py
+++ main.py
@@ -7,45 +7,57 @@ Created on Sat Apr 25 17:48:47 2020
 
 
 import sys
-from math import cos,sin
+from math import cos, sin, pi
 from time import sleep
 
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtCore import Qt
+from PyQt5 import QtGui, QtWidgets
 
 import mainwindow
 
 # R = 125 # радиус внешней окружности
 # r = 70 # радиус внутренней окружности
 # d = 25 # длина плеча
-m = 3 # scale multiplyer
+M = 3 # scale multiplyer
 
 pen_outer = QtGui.QPen(QtGui.QColor('red'))
 pen_inner = QtGui.QPen(QtGui.QColor('blue'))
 pen_plecho = QtGui.QPen(QtGui.QColor('green'))
 pen_spiro = QtGui.QPen(QtGui.QColor('black'))
 pen_spiro.setWidth(4)
+PEN_CLEAR = QtGui.QPen(QtGui.QColor('white'))
 
 
-def draw_circle(self, x, y, r):
-    self.drawEllipse(int(x - r), int(y - r), r * 2, r * 2)
+def draw_circle(self, center_x, center_y, radius):
+    self.drawEllipse(
+        int(center_x - radius),
+        int(center_y - radius),
+        radius * 2,
+        radius * 2)
 
 class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
     def __init__(self):
         super().__init__()
 
         self.setupUi(self)
-        self.label_out_final.setVisible(False)
+        _l = QtWidgets.QStackedLayout()
+        _l.addWidget(self.label_out_final)
+        _l.addWidget(self.label_out)
+        _l.addWidget(self.label_bg)
+        _l.setStackingMode(QtWidgets.QStackedLayout.StackAll)
+        self.mwLayout_2.addLayout(_l)
+#        self.label_out_final.setVisible(False)
         self.but_save.clicked.connect(self.save_file)
         self.but_draw.clicked.connect(self.draw_image)
 
-        canvas = QtGui.QPixmap(400 * m, 400 * m)
-        canvas.fill(QtGui.QColor('#ffffff'))
-
-        canvas_final = QtGui.QPixmap(400 * m, 400 * m)
-        canvas_final.fill(QtGui.QColor('#ffffff'))
+        canvas = QtGui.QPixmap(400 * M, 400 * M)
+        canvas.fill(QtGui.QColor('white'))
+        canvas_bg = QtGui.QPixmap(400 * M, 400 * M)
+        canvas_bg.fill(QtGui.QColor('white'))
+        canvas_final = QtGui.QPixmap(400 * M, 400 * M)
+        canvas_final.fill(QtGui.QColor('transparent'))
 
         self.label_out.setPixmap(canvas)
+        self.label_bg.setPixmap(canvas_bg)
         self.label_out_final.setPixmap(canvas_final)
         self.update()
 
@@ -53,53 +65,75 @@ class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         return
 
     def draw_image(self):
-        R = self.spinBox_outer_diameter.value() * m
-        r = self.spinBox_inner_diameter.value() * m
-        d = self.spinBox_plecho.value() * m
-        theta = 0.2
-        steps = int(6*3.14/theta)
+        r_o = self.spinBox_outer_diameter.value() * M
+        r_i = self.spinBox_inner_diameter.value() * M
+        d = self.spinBox_plecho.value() * M
+        theta = 0.02
         angle = 0
+        _inner_center_x_0 = (r_o - r_i) * cos(angle)
+        _inner_center_y_0 = (r_o - r_i) * sin(angle)
 
-        for t in range(steps):
-            _inner_center_x = (R - r) * cos(angle)
-            _inner_center_y = (R - r) * sin(angle)
+        _spiro_x = 0
+        _spiro_y = 0
 
-            _spiro_x = _inner_center_x + d * cos(((R-r)/r)*angle)
-            _spiro_y = _inner_center_y - d * sin(((R-r)/r)*angle)
+        while int(angle) != int(2 * pi / theta):
+            _inner_center_x = (r_o - r_i) * cos(angle)
+            _inner_center_y = (r_o - r_i) * sin(angle)
+
+            _spiro_x_new = _inner_center_x + d * cos(((r_o-r_i)/r_i)*angle)
+            _spiro_y_new = _inner_center_y - d * sin(((r_o-r_i)/r_i)*angle)
+
+            self.label_out.pixmap().fill(QtGui.QColor('#ffffff'))
 
             _p = QtGui.QPainter(self.label_out.pixmap())
-            _p.translate(200 * m, 200 * m)
+            _p.translate(200 * M, 200 * M)
 
             _p.setPen(pen_outer)
             _p.drawPoint(0, 0)
-            draw_circle(_p, 0, 0, R)
+            draw_circle(_p, 0, 0, r_o)
 
             _p.setPen(pen_inner)
             _p.drawPoint(_inner_center_x, _inner_center_y)
-            draw_circle(_p, _inner_center_x, _inner_center_y, r)
+            draw_circle(_p, _inner_center_x, _inner_center_y, r_i)
 
             _p.setPen(pen_plecho)
-            _p.drawLine(_inner_center_x, _inner_center_y, _spiro_x, _spiro_y)
+            _p.drawLine(_inner_center_x,
+                        _inner_center_y,
+                        _spiro_x_new,
+                        _spiro_y_new)
 
-            _p.setPen(pen_spiro)
-            _p.drawPoint(_spiro_x, _spiro_y)
-            _p.end()
+#            _p.setPen(pen_spiro)
+#            _p.drawPoint(_spiro_x, _spiro_y)
 
-            _p_final = QtGui.QPainter(self.label_out_final.pixmap())
-            _p_final.translate(200 * m, 200 * m)
-            _p_final.setPen(pen_spiro)
-            _p_final.drawPoint(_spiro_x, _spiro_y)
-            _p_final.end()
+            if _spiro_x != 0 and _spiro_y != 0:
+                _p_final = QtGui.QPainter(self.label_out_final.pixmap())
+                _p_final.translate(200 * M, 200 * M)
+                _p_final.setPen(pen_spiro)
+                _p_final.drawLine(
+                    _spiro_x,
+                    _spiro_y,
+                    _spiro_x_new,
+                    _spiro_y_new)
+                _p_final.end()
 
-            sleep(0.01)
+            _spiro_x = _spiro_x_new
+            _spiro_y = _spiro_y_new
+
             angle += theta
-            self.label_out.update()
-            self.label_out.setVisible(True)
-            print('updating %s' % t)
+            self.label_out.repaint()
+
+#            _p.setPen(PEN_CLEAR)
+#            draw_circle(_p, _inner_center_x, _inner_center_y, r_i)
+#            _p.drawLine(_inner_center_x, _inner_center_y, _spiro_x, _spiro_y)
+
+            _p.end()
+#            print(angle)
+
+            sleep(0.001)
 
         sleep(1)
         self.label_out.setVisible(False)
-        self.label_out_final.setVisible(True)
+#        self.label_out_final.setVisible(True)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
